665d3cc620851ee2c3955dfaf2f543cf5ba8f6e3
rkm10 <rajkumarmalluri08@gmail.com>
2024-05-30 18:47:52 +0530

    projects updated

--- javascrpt/Promises.js
+++ javascrpt/Promises.js
@@ -1,53 +1,53 @@
-// function getData(dataId) {
-//       return new Promise((resolve, reject) => {
-//             setTimeout(() => {
-//                   console.log("data", dataId);
-//                   resolve("success")
-//             }, 4000)
-//       })
-// }
-// function getData1(dataId) {
-//       return new Promise((resolve, reject) => {
-//             setTimeout(() => {
-//                   console.log("data", dataId);
-//                   resolve("success")
-//             }, 1000)
-//       })
-// }
+function getData(dataId) {
+      return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                  console.log("data", dataId);
+                  resolve("success")
+            }, 4000)
+      })
+}
+function getData1(dataId) {
+      return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                  console.log("data", dataId);
+                  resolve("success")
+            }, 1000)
+      })
+}
 
 // //----------1st method----///
 
-// console.log("fecthing data 1......");
-// let p1 = getData(2)
-// p1.then((res) => {
-//       console.log(res);
-//       console.log("fecthcing data2 .....");
-//       let p2 = getData(3)
-//       p2.then((res) => {
-//             console.log(res)
-//       })
-// })
+console.log("fecthing data 1......");
+let p1 = getData(2)
+p1.then((res) => {
+      console.log(res);
+      console.log("fecthcing data2 .....");
+      let p2 = getData(3)
+      p2.then((res) => {
+            console.log(res)
+      })
+})
 
 // //----------2nd method----///
 
-// console.log("fecthing data 1......");
-// let p3 = getData1(2)
-// p3.then((res) => {
-//       console.log(res);
-//       console.log("fecthcing data2 .....");
-//       let p4 = getData1(3)
-//       p4.then((res) => {
-//             console.log(res)
-//       })
-// })
+console.log("fecthing data 1......");
+let p3 = getData1(2)
+p3.then((res) => {
+      console.log(res);
+      console.log("fecthcing data2 .....");
+      let p4 = getData1(3)
+      p4.then((res) => {
+            console.log(res)
+      })
+})
 
 // //---------- simplified 3rd method----///
 
-// console.log("fecthing data 1......");
-// getData1(2).then((res) => {
-//       console.log("fecthcing data2 .....");
-//       getData1(3).then((res) => { })
-// })
+console.log("fecthing data 1......");
+getData1(2).then((res) => {
+      console.log("fecthcing data2 .....");
+      getData1(3).then((res) => { })
+})
 
 // //--------------------------//
 
@@ -64,13 +64,13 @@ function gettingData(dataId, getNextData) {
 
 //-------method 1--------//
 
-// console.log("data is fetching.......");
-// gettingData(1).then((res) => {
-//       console.log(res);
-//       gettingData(2).then((res) => {
-//             console.log(res);
-//       })
-// })
+console.log("data is fetching.......");
+gettingData(1).then((res) => {
+      console.log(res);
+      gettingData(2).then((res) => {
+            console.log(res);
+      })
+})
 
 //-------simplifed way method 1--------//
 
